7647155c7183085f7dd432d3aa18441c3de27282
michaelzheng-joyce101 <michael.zheng@mymail.champlain.edu>
2020-02-03 15:11:39 -0500

    Initial
    Created unreal project

---
+++ WaveFunctionCollapse/Intermediate/Build/Win64/WaveFunctionCollapseEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/michael.zheng/Desktop/Console/WFC_Tool/WaveFunctionCollapse/Intermediate/Build/Win64/WaveFunctionCollapseEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ WaveFunctionCollapse/Source/WaveFunctionCollapse/WaveFunctionCollapse.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+//base wave function callapse
+// needs tile ustruct that contains the data such as tile position and the tile name
+//need a function library for the WFC
+//a static WFC algorithm in here that will generate it.
+//all other nessassary WFC functions and classes will go in here
+
+class WaveFunctionCollapseClass
+{
+	//
+};
