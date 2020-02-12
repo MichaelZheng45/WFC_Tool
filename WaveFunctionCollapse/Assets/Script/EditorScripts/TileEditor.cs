c2b47e0abaed14dff66f1e5fb993854f3976aea5
michael  zheng fosterHall <michael.zheng@mymail.champlain.edu>
2020-02-12 12:18:23 -0500

    added unity project

---
+++ WaveFunctionCollapse/Assets/Script/EditorScripts/TileEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class TileEditor : EditorWindow
+{
+    [MenuItem("Window/Tile Editor")]
+    static void Init()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+    }
+}
---
+++ WaveFunctionCollapse/Assets/Script/WaveFunctionCollapse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFunctionCollapse
+{
+    public WaveFunctionCollapse()
+    {
+
+    }
+
+    public bool run()
+    {
+        return false;
+    }
+
+    void init()
+    {
+
+    }
+
+    void clear()
+    {
+
+    }
+
+    void observe()
+    {
+
+    }
+}
+
