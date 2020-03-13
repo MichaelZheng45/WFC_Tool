04fa016ce84666b1847dcffbf51a48ecc9aa4d76
Zheng <michael.zheng@champlain.edu>
2020-03-13 14:44:23 -0400

    moved files etc.

---
+++ WFCT/Assets/Script/EditorScripts/SampleMapEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SampleMapEditor : EditorWindow
+{ 
+    int sizeX;
+    int sizeY;
+    sampleMap sMap;
+    [MenuItem("Window/Sample Map Editor")]
+    static void Init()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        
+    }
+
+
+}
